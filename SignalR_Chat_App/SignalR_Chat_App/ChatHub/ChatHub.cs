b71b24974c1595d85bca4b3285c29ade9eec2245
upesic1512 <108237723+upesic1512@users.noreply.github.com>
2024-10-29 20:52:22 +0100

    Add project files.

---
+++ SignalR_Chat_App/SignalR_Chat_App/ChatHub/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR_Chat_App.ChatHub
+{
+    public class ChatHub: Hub
+    {
+        public async Task SendMessage(string user_name, string message, DateTime date)
+            => await Clients.All.SendAsync("receive_message", user_name, message, date);
+
+    }
+}
---
+++ SignalR_Chat_App/SignalR_Chat_App/Program.cs
@@ -0,0 +1,38 @@
+using SignalR_Chat_App.ChatHub;
+using SignalR_Chat_App.Client.Pages;
+using SignalR_Chat_App.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(SignalR_Chat_App.Client._Imports).Assembly);
+
+app.MapHub<ChatHub>("/chathub");
+
+app.Run();
